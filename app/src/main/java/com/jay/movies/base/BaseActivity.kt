fb0513cc4141f66d2a174bdfe711383cf06d0742
Jihoi-Kang <jihoi.kang@gmail.com>
2020-11-20 17:00:32 +0900

    Add base activity

---
+++ app/src/main/java/com/jay/movies/base/BaseActivity.kt
@@ -0,0 +1,19 @@
+package com.jay.movies.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<B : ViewDataBinding>(
+    @LayoutRes private val layoutResId: Int,
+) : AppCompatActivity() {
+
+    protected lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView<B>(this, layoutResId)
+    }
+}
