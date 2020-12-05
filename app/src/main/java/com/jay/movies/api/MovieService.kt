e459faf74fb56c974dcb4e60b1e3ccac9bab1e31
Jihoi-Kang <jihoi.kang@gmail.com>
2020-12-05 19:01:20 +0900

    Add retrofit query parameter interceptor

--- app/src/main/java/com/jay/movies/api/MovieService.kt
+++ app/src/main/java/com/jay/movies/api/MovieService.kt
@@ -1,6 +1,5 @@
 package com.jay.movies.api
 
-import com.jay.movies.BuildConfig
 import com.jay.movies.api.response.GenreResponse
 import com.jay.movies.api.response.MovieResponse
 import com.jay.movies.api.response.VideoResponse
@@ -17,18 +16,14 @@ interface MovieService {
         @Query("sort_by") sortBy: String,
         @Query("page") page: Int,
         @Query("release_date.lte") today: String = SimpleDateFormat("yyyy-MM-dd").format(Date()),
-        @Query("api_key") apiKey: String = BuildConfig.TMDB_API_KEY,
     ): MovieResponse
 
     @GET("/3/genre/movie/list")
-    suspend fun fetchMovieGenres(
-        @Query("api_key") apiKey: String = BuildConfig.TMDB_API_KEY,
-    ): GenreResponse
+    suspend fun fetchMovieGenres(): GenreResponse
 
     @GET("/3/movie/{movie_id}/videos")
     suspend fun fetchMovieVideos(
         @Path("movie_id") id: Int,
-        @Query("api_key") apiKey: String = BuildConfig.TMDB_API_KEY,
     ): VideoResponse
 
 }
--- app/src/main/java/com/jay/movies/di/NetworkModule.kt
+++ app/src/main/java/com/jay/movies/di/NetworkModule.kt
@@ -1,5 +1,6 @@
 package com.jay.movies.di
 
+import com.jay.movies.BuildConfig
 import com.jay.movies.api.Api.BASE_TMDB_API_PATH
 import com.jay.movies.api.MovieService
 import dagger.Module
@@ -7,10 +8,13 @@ import dagger.Provides
 import dagger.Reusable
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Request
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+
 
 @InstallIn(ApplicationComponent::class)
 @Module
@@ -23,7 +27,20 @@ class NetworkModule {
             level = HttpLoggingInterceptor.Level.BASIC
         }
 
+        val queryParameter = Interceptor { chain ->
+            val original = chain.request()
+            val url = original.url.newBuilder()
+                .addQueryParameter("api_key", BuildConfig.TMDB_API_KEY)
+                .build()
+
+            val requestBuilder: Request.Builder = original.newBuilder().url(url)
+
+            val request: Request = requestBuilder.build()
+            chain.proceed(request)
+        }
+
         val client = OkHttpClient.Builder()
+            .addInterceptor(queryParameter)
             .addInterceptor(logger)
             .build()
 
