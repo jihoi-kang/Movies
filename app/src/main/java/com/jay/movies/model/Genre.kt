34409e9b75b2283b262ab8b7a3fea7a4e8640a5b
Jihoi-Kang <jihoi.kang@gmail.com>
2020-12-08 19:44:04 +0900

    Remove room dependency

--- app/src/main/java/com/jay/movies/model/Genre.kt
+++ app/src/main/java/com/jay/movies/model/Genre.kt
@@ -1,11 +1,9 @@
 package com.jay.movies.model
 
 import android.os.Parcelable
-import androidx.room.Entity
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-@Entity(primaryKeys = [("id")])
 data class Genre(
     val id: Int,
     val name: String,
--- app/src/main/java/com/jay/movies/model/Video.kt
+++ app/src/main/java/com/jay/movies/model/Video.kt
@@ -1,11 +1,9 @@
 package com.jay.movies.model
 
 import android.os.Parcelable
-import androidx.room.Entity
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-@Entity(primaryKeys = [("id")])
 data class Video(
     val id: String,
     val name: String,
