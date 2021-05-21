d36ea2b45a62dd129989f95788f5f3a08a0fc420
Jihoi-Kang <jihoi.kang@gmail.com>
2021-05-21 16:32:50 +0900

    Remove movie result

--- app/src/main/java/com/jay/movies/api/MovieResult.kt
+++
@@ -1,8 +0,0 @@
-package com.jay.movies.api
-
-import com.jay.movies.model.Movie
-
-sealed class MovieResult {
-    data class Success(val data: List<Movie>) : MovieResult()
-    data class Error(val error: Exception) : MovieResult()
-}
